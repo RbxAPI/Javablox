1dd68e731d3ee36ebc16b79d84ae53a1303d43a0
Unknown <wongchingho0@gmail.com>
2018-09-08 16:30:49 +0800

    PrivacySettings API

--- src/main/java/io/github/tclrainbow/Javablox/api/accountsettings/privacysettings/IInventoryPrivacy.java
+++ src/main/java/io/github/tclrainbow/Javablox/api/accountsettings/privacysettings/IInventoryPrivacy.java
@@ -12,4 +12,18 @@ public interface IInventoryPrivacy {
      * }
      */
     String getInventoryPrivacy();
+
+    /**
+     * Updates a user's inventory privacy setting
+     * https://accountsettings.roblox.com/docs#!/PrivacySettings/post_v1_inventory_privacy
+     * @param model {
+     *   "inventoryPrivacy": "NoOne"
+     * }
+     * @return {
+     *   "inventoryPrivacy": "NoOne",
+     *   "tradePrivacy": "All",
+     *   "privacySettingResponse": "Success"
+     * }
+     */
+    String setInventoryPrivacy(String model);
 }
