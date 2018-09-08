f1431912fa518e3f3505e6d6d2196c4ecb2bcbc0
Pythonic-Rainbow <wongchingho0@gmail.com>
2018-09-08 23:40:15 +0800

    Group ID update

---
+++ src/main/java/io/github/rbxapi/Javablox/api/accountsettings/tradesettings/ITradePrivacy.java
@@ -0,0 +1,29 @@
+package io.github.rbxapi.Javablox.api.accountsettings.tradesettings;
+
+/**
+ * https://accountsettings.roblox.com/docs#/TradeSettings
+ */
+public interface ITradePrivacy {
+    /**
+     * Get a user's trade privacy setting
+     * https://accountsettings.roblox.com/docs#!/TradeSettings/get_v1_trade_privacy
+     * @return {
+     *   "tradePrivacy": "string"
+     * }
+     */
+    String getTradePrivacy();
+
+    /**
+     * Updates a user's trade privacy setting
+     * https://accountsettings.roblox.com/docs#!/TradeSettings/post_v1_trade_privacy
+     * @param model {
+     *   "tradePrivacy": "All"
+     * }
+     * @return {
+     *   "tradePrivacy": "All",
+     *   "inventoryPrivacy": "NoOne",
+     *   "privacySettingResponse": "Success"
+     * }
+     */
+    String setTradePrivacy(String model);
+}
