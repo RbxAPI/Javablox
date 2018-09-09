d634f22c2f3ef413e4051f505707004c5cde5280
Pythonic-Rainbow <wongchingho0@gmail.com>
2018-09-09 12:43:08 +0800

    Billing API

---
+++ src/main/java/io/github/rbxapi/Javablox/api/billing/payments/IPayment.java
@@ -0,0 +1,43 @@
+package io.github.rbxapi.Javablox.api.billing.payments;
+
+/**
+ * https://billing.roblox.com/docs#/Payments
+ */
+public interface IPayment {
+
+    /**
+     * Retrieve the payment history for Authenticated user
+     * https://billing.roblox.com/docs#!/Payments/get_v1_user_payments
+     * @param sortOrder (asc/desc) Sorted by paymentDate
+     * @param limit (enum) The amount of results per request.
+     * @param cursor The paging cursor for the previous or next page.
+     * @return {
+     *   "previousPageCursor": "string",
+     *   "nextPageCursor": "string",
+     *   "data": [
+     *     {
+     *       "paymentDate": "2018-09-09T04:05:08.705Z",
+     *       "products": [
+     *         {
+     *           "name": "string"
+     *         }
+     *       ],
+     *       "money": {
+     *         "Amount": 0,
+     *         "USDAmount": 0,
+     *         "Currency": {
+     *           "Id": 0,
+     *           "CurrencyType": "USD",
+     *           "CurrencyName": "string",
+     *           "CurrencySymbol": "string"
+     *         }
+     *       },
+     *       "paymentProviderType": "AppleAppStore",
+     *       "creditCardType": "Visa",
+     *       "cardNumber": "string"
+     *     }
+     *   ]
+     * }
+     */
+    String getPaymentHistory(String sortOrder, int limit, String cursor);
+}
