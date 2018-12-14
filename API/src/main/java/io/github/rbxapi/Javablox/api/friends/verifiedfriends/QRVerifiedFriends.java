90db41838153aba158a11cf2f022fc1b1e6b4351
Pythonic-Rainbow <wongchingho0@gmail.com>
2018-12-14 18:54:00 +0800

    Friends.Verifiedfriends

---
+++ API/src/main/java/io/github/rbxapi/Javablox/api/friends/verifiedfriends/QRVerifiedFriends.java
@@ -0,0 +1,16 @@
+package io.github.rbxapi.Javablox.api.friends.verifiedfriends;
+
+public interface QRVerifiedFriends {
+
+    /**
+     * https://friends.roblox.com/docs#!/VerifiedFriends/delete_v1_friends_verified_qr_session
+     * @return Status code
+     */
+    String deleteSession();
+
+    /**
+     * https://friends.roblox.com/docs#!/VerifiedFriends/post_v1_friends_verified_qr_session
+     * @return Status code
+     */
+    String createSession();
+}
