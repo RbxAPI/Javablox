c0d8db5619f2ea8648205352f8201204195e3d9c
Pythonic-Rainbow <wongchingho0@gmail.com>
2018-12-23 15:21:26 +0800

    Game Internationalization API

---
+++ API/src/main/java/io/github/rbxapi/Javablox/api/gameinternationalization/NameDescription.java
@@ -0,0 +1,18 @@
+package io.github.rbxapi.Javablox.api.gameinternationalization;
+
+public interface NameDescription {
+    /**
+     * https://gameinternationalization.roblox.com/docs#!/NameDescription/get_v1_name_description_games_gameId
+     * @param gameId Game ID
+     * @return Details
+     */
+    String getNameDescription(double gameId);
+
+    /**
+     * https://gameinternationalization.roblox.com/docs#!/NameDescription/patch_v1_name_description_games_gameId
+     * @param gameId Game ID
+     * @param request Request body
+     * @return JSON status body
+     */
+    String setNameDescription(double gameId, String request);
+}
