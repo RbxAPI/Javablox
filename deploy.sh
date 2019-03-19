e349631f38045e2aba76ff669aa9d919fe980a50
Pythonic-Rainbow <wongchingho0@gmail.com>
2019-03-19 17:19:45 +0800

    Encryption on ubuntu-windows

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 echo $mavenUser
 echo ${mavenUser}
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K $encrypted_1a499bc70cbf_key -iv $encrypted_1a499bc70cbf_iv -in travis.gpg.enc -out travis.gpg -d
+  openssl aes-256-cbc -K $encrypted_b8b4d31050a3_key -iv $encrypted_b8b4d31050a3_iv -in travis.gpg.enc -out travis.gpg -d
   ./gradlew publish -PossrhUsername=${mavenUser} -PossrhPassword=${mavenPassword} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=travis.gpg
 fi
