594f369db57853102969d91b750226ec17d7db8e
Pythonic-Rainbow <wongchingho0@gmail.com>
2019-03-30 15:32:56 +0800

    Update Test settings

--- javablox-endpoint/src/main/java/module-info.java
+++ javablox-endpoint/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module io.github.rbxapi.javablox.endpoint {
+    exports io.github.rbxapi.javablox.endpoint.accountsetting;
+
     requires io.github.rbxapi.javablox.api;
     requires java.net.http;
 }
